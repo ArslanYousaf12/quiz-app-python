209bdada39d61e5ca686d13353af4941fb1ea82a
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-03-07 12:45:18 +0500

    question bank added

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-class User:
-    def __init__(self, user_id, user_name, ):
-        self.id = user_id
-        self.username = user_name
-        self.followers = 0
-        self.following = 0
-        print("Constructor function calling")
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    text = question["text"]
+    answer = question["answer"]
+    question_bank.append(Question(text, answer))
 
 
-user_1 = User(user_id=123, user_name="Arslan")
-
-
-print(user_1.username)
-user_2 = User(user_id=2223, user_name="kamran")
-
-print(user_2.username)
-user_1.follow(user_2)
-print(user_1.following)
-print(user_2.followers)
+print(question_bank[3].text)
