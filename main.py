5c9250e3bc6dd173076d37f0fda31ebeda756979
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-03-08 12:17:01 +0500

    quiz complete

--- main.py
+++ main.py
@@ -12,5 +12,9 @@ for question in question_data:
 
 print(question_bank[3].text)
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You Completed the quiz")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
 
